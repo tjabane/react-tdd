26211c5c444120a20d3eaf25e9cece88e2b9080b
Tiisetso Tjabane <t.tjabane@gmail.com>
2020-11-09 22:00:36 +0200

    read tdd example

---
+++ src/components/NewMessageForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const NewMessageForm = ({ onSend }) => {
+    const [inputText, setInputText] = useState('');
+    const handleTextChange = event => {
+           setInputText(event.target.value);
+    };
+
+    const handleSend = () => {
+        onSend(inputText);
+        setInputText('');
+    }
+    return (
+        <div>
+            <input  type="text" 
+                    data-testid="messageText"
+                    value={inputText}
+                    onChange={handleTextChange}
+                />
+            <button onClick={handleSend} 
+                    data-testid="sendButton"
+                > Send </button>
+        </div>
+    );
+};
+
+export default NewMessageForm;
